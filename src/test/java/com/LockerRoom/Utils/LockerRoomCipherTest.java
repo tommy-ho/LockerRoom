01c55f4befad82098a5be3c607a6b9e6a9f159ea
Tommy <Tommy@DESKTOP-ATVKQSF>
2017-08-02 21:43:13 -0400

    Converted to Maven project

---
+++ src/test/java/com/LockerRoom/Utils/LockerRoomCipherTest.java
@@ -0,0 +1,16 @@
+package com.LockerRoom.Utils;
+
+import org.junit.Test;
+
+public class LockerRoomCipherTest {
+
+	public LockerRoomCipherTest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Test
+	public void encryptPWreturns16ByteString(){
+		
+	}
+
+}
